008ab35b05de3ee94ff1ec48cc754767e70c261a
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-08-08 16:46:49 -0400

    Receive props to child component

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -2,21 +2,27 @@ import styles from '../styles/styles.module.css'
 import noImage from '../assets/no-image.jpg'
 import useProduct from '../hooks/useProduct'
 
+interface Props {
+    product: Product
+}
 
-export const ProductCard = () => {
+interface Product {
+    id: string,
+    title: string,
+    img?: string
+}
+
+export const ProductCard = ({ product }: Props) => {
     const { counter, increaseBy} = useProduct()
 
   return (
     <div className={styles.productCard}>
   <img
         className={styles.productImg}
-        src='/images/coffee-mug.png'
+        src={product.img ? product.img : noImage}
         alt='Coffe Mug'/>
-        {/* <img
-        className={styles.productImg}
-        src={noImage}
-        alt='Coffe Mug'/> */}
-        <span className={styles.productDescription}>Coffe Mug</span>
+
+        <span className={styles.productDescription}>{product.title}</span>
         <div className={styles.buttonsContainer}>
             <button
             className={styles.buttonMinus}
