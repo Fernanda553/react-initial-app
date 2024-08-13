06538dd6301f90e6363bdc9afb7a5dbe0b889f33
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-08-13 11:43:05 -0400

    Custom className

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,13 +1,19 @@
 import styles from '../styles/styles.module.css'
 import useProduct from '../hooks/useProduct'
-import { createContext, Dispatch, useContext } from 'react'
-import { ProductCardProps, ProductContextProps} from '../interfaces/interfaces'
+import { createContext, Dispatch, ReactElement, useContext } from 'react'
+import { Product, ProductContextProps} from '../interfaces/interfaces'
 
   export const ProductContext = createContext({} as ProductContextProps)
 
     const { Provider } = ProductContext
 
-export const ProductCard = ({children, product }: ProductCardProps) => {
+    export interface Props{
+      product: Product
+      children?: ReactElement | ReactElement[]
+      className?: string
+  }
+
+export const ProductCard = ({children, product , className}: Props) => {
     const { counter, increaseBy} = useProduct()
 
   return (
@@ -16,7 +22,7 @@ export const ProductCard = ({children, product }: ProductCardProps) => {
         increaseBy,
         product
     }}>
-            <div className={styles.productCard}>
+            <div className={`${ styles.productCard } ${className}`}>
         {children}
 {/*         <ProductImage img={product.img}/>
        <ProductTitle title={product.title}/>
