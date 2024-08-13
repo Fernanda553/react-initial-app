ae347173135c15059ab828a7d096ec546a80796f
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-08-13 10:54:02 -0400

    Assign components to another component

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -16,6 +16,12 @@ export const ShoppingPage = () => {
           flexDirection: 'row',
           flexWrap: 'wrap'
         }}>
+          <ProductCard product={product}>
+        <ProductCard.Image/>
+        <ProductCard.Title title={'Hola Mundo'}/>
+        <ProductCard.Buttons/>
+          </ProductCard>
+
         <ProductCard product={product}>
           <ProductImage/>
           <ProductTitle title={''}/>
