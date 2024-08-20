766471ef9c29665b3bf9847847f214dfb767b9e4
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-08-20 14:39:02 -0400

    Sync from cart to main cards

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -42,6 +42,7 @@ export const ShoppingPage = () => {
     })
   }
 
+
   return (
     <div>
         <h1>Shopping Store</h1>
@@ -58,6 +59,7 @@ export const ShoppingPage = () => {
        product={product}
        className="bg-dark text-white"
        onChange={onProductCountChange}
+       value={shoppingCart[product.id]?.count || 0}
        >
        <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
        <ProductTitle className="text-bold"/>
@@ -81,6 +83,7 @@ export const ShoppingPage = () => {
                   product={product}
                   className="bg-dark text-white"
                   style={{ width:'100px' }}
+                  onChange={onProductCountChange}
                   value={product.count}
                   >
                   <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
