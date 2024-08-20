000b33808f68d737558985aeea4d129327d69767
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-08-20 13:56:40 -0400

    Show shopping cart items

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -61,37 +61,33 @@ export const ShoppingPage = () => {
        >
        <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
        <ProductTitle className="text-bold"/>
-       <ProductButtons className="custom-buttons"/>
+       <ProductButtons
+       className="custom-buttons"
+       style={{
+        display:'flex',
+        justifyContent: 'center'
+       }}
+       />
      </ProductCard>
           ))}
 
         </div>
+
         <div className="shopping-cart">
-        <ProductCard
-       product={product2}
-       className="bg-dark text-white"
-       style={{ width:'100px' }}
-       >
-       <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
-       <ProductTitle className="text-bold"/>
-       <ProductButtons className="custom-buttons"/>
-     </ProductCard>
 
-     <ProductCard
-       product={product}
-       className="bg-dark text-white"
-       style={{ width:'100px' }}
-       >
-       <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
-       <ProductTitle className="text-bold"/>
-       <ProductButtons className="custom-buttons"/>
-     </ProductCard>
-        </div>
+        { Object.entries(shoppingCart).map(([key, product]) => (
+                  <ProductCard
+                  key={key}
+                  product={product}
+                  className="bg-dark text-white"
+                  style={{ width:'100px' }}
+                  >
+                  <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
+                  <ProductTitle className="text-bold"/>
+                  <ProductButtons className="custom-buttons"/>
+                </ProductCard>
+        ))}
 
-        <div>
-          <code>
-            {JSON.stringify(shoppingCart, null, 5)}
-          </code>
         </div>
 
     </div>
