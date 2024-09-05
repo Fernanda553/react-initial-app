14273765efeff573f0c290cc6f95c640abf1f43d
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-09-05 15:19:27 -0400

    Traditional form - with useState

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,6 +1,19 @@
+import { ChangeEvent, useState } from 'react'
 import '../styles/styles.css'
 
 const RegisterPage = () => {
+    const [ registerData, setRegisterData ] = useState({
+        name: 'fernanda',
+        email: 'fernanda@gmail.com',
+        password1: '123456',
+        password2: '123456'
+    })
+
+    const { name, email, password1, password2 } = registerData
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+      console.log(event.target.value)
+    }
   return (
     <div>
         <h1>Register Page</h1>
@@ -9,21 +22,29 @@ const RegisterPage = () => {
             <input
             type="text"
             placeholder="Name"
+            value={name}
+            onChange={onChange}
             />
 
             <input
             type="email"
             placeholder="Email"
+            value={email}
+            onChange={onChange}
             />
 
             <input
             type="password"
             placeholder="Password"
+            value={password1}
+            onChange={onChange}
             />
 
             <input
             type="password"
             placeholder="Repeat password"
+            value={password2}
+            onChange={onChange}
             />
 
             <button type="submit">Create</button>
