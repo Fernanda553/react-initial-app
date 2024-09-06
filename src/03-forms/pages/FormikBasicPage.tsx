4804556256acdbc620e1e78765db8ebaf5e196b7
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-09-06 10:10:25 -0400

    Formik - Manual validations

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,7 +1,37 @@
-import { useFormik } from 'formik'
+import { FormikErrors, useFormik } from 'formik'
 import '../styles/styles.css'
 
+interface FormValues {
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
 const FormikBasicPage = () => {
+  const validate = ({ firstName, lastName, email }: FormValues) => {
+    const errors: FormikErrors<FormValues>={}
+
+    if(!firstName){
+      errors.firstName = 'Required'
+    }else if(firstName.length >= 15){
+      errors.firstName = 'Must be 15 characters or less'
+    }
+
+    if(!lastName){
+      errors.lastName = 'Required'
+    }else if(lastName.length >= 10){
+      errors.lastName = 'Must be 10 characters or less'
+    }
+
+    if (!email) {
+      errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors
+  }
+
   const {handleChange, handleSubmit, values} = useFormik({
     initialValues:{
       firstName: '',
@@ -10,8 +40,10 @@ const FormikBasicPage = () => {
     },
     onSubmit:(values) =>{
       console.log(values)
-    }
+    },
+    validate
   })
+
   return (
     <div>
         <h1>Formik Basic Tutorial</h1>
@@ -25,7 +57,7 @@ const FormikBasicPage = () => {
         />
         <span>First name is required</span>
 
-        <label htmlFor='firstName'>Last Name</label>
+        <label htmlFor='lastName'>Last Name</label>
         <input
         type='text'
         name='lastName'
@@ -35,7 +67,7 @@ const FormikBasicPage = () => {
         <span>Last name is required</span>
         <span>Check for an valid email format</span>
 
-        <label htmlFor='firstName'>Email Address</label>
+        <label htmlFor='email'>Email Address</label>
         <input
         type='email'
         name='email'
