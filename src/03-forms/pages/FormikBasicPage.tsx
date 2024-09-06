1709a8d46c563ac76f385e5b5997a5ed48e5647a
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-09-06 13:42:37 -0400

    Formik - Show errors in fields

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -32,7 +32,7 @@ const FormikBasicPage = () => {
     return errors
   }
 
-  const {handleChange, handleSubmit, values} = useFormik({
+  const {handleChange, handleSubmit, handleBlur ,touched, values, errors } = useFormik({
     initialValues:{
       firstName: '',
       lastName: '',
@@ -52,29 +52,31 @@ const FormikBasicPage = () => {
         <input
         type='text'
         name='firstName'
+        onBlur={handleBlur}
         onChange={handleChange}
         value={values.firstName}
         />
-        <span>First name is required</span>
+        {touched.firstName && errors.firstName && <span>{errors.firstName}</span>}
 
         <label htmlFor='lastName'>Last Name</label>
         <input
         type='text'
         name='lastName'
+        onBlur={handleBlur}
         onChange={handleChange}
         value={values.lastName}
         />
-        <span>Last name is required</span>
-        <span>Check for an valid email format</span>
+        {touched.lastName && errors.lastName && <span>{errors.lastName}</span>}
 
         <label htmlFor='email'>Email Address</label>
         <input
         type='email'
         name='email'
+        onBlur={handleBlur}
         onChange={handleChange}
         value={values.email}
         />
-        <span>Email is required</span>
+        {touched.email && errors.email && <span>{errors.email}</span>}
         <button type='submit'>Submit</button>
         </form>
     </div>
