0a3d21dbeb5f3279e0685115ed691143bc83d192
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-09-06 09:42:05 -0400

    Formik - Get form information

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,14 +1,27 @@
+import { useFormik } from 'formik'
 import '../styles/styles.css'
 
 const FormikBasicPage = () => {
+  const {handleChange, handleSubmit, values} = useFormik({
+    initialValues:{
+      firstName: '',
+      lastName: '',
+      email:''
+    },
+    onSubmit:(values) =>{
+      console.log(values)
+    }
+  })
   return (
     <div>
         <h1>Formik Basic Tutorial</h1>
-        <form noValidate>
+        <form onSubmit={handleSubmit} noValidate>
         <label htmlFor='firstName'>First Name</label>
         <input
         type='text'
         name='firstName'
+        onChange={handleChange}
+        value={values.firstName}
         />
         <span>First name is required</span>
 
@@ -16,6 +29,8 @@ const FormikBasicPage = () => {
         <input
         type='text'
         name='lastName'
+        onChange={handleChange}
+        value={values.lastName}
         />
         <span>Last name is required</span>
         <span>Check for an valid email format</span>
@@ -24,6 +39,8 @@ const FormikBasicPage = () => {
         <input
         type='email'
         name='email'
+        onChange={handleChange}
+        value={values.email}
         />
         <span>Email is required</span>
         <button type='submit'>Submit</button>
