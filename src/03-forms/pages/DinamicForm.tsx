933ed2a33e5f34ca2f9538a075414dc08c93036e
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-09-10 21:26:39 -0300

    Creating the initialValues ​​dynamically

--- src/03-forms/pages/DinamicForm.tsx
+++ src/03-forms/pages/DinamicForm.tsx
@@ -2,15 +2,19 @@ import { Formik, Form } from 'formik'
 import formJson from '../data/custom-form.json'
 import { MyTextInput } from '../components'
 
+const initialValues: {[x: string]: any} = {}
+
+for(const input of formJson){
+  initialValues[input.name] = input.value
+}
+
 const DinamicForm = () => {
   return (
     <div>
         <h1>DinamicForm</h1>
 
         <Formik
-          initialValues={{
-            name: ""
-          }}
+          initialValues={initialValues}
           onSubmit={(values) => {
             console.log(values)
           }}
